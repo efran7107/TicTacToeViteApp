17f5870dd128d435e7516803efc0af6c4920f0c2
efran7107 <efran7107@gmail.com>
2024-11-04 05:59:26 -0700

    moving to pc

--- src/components/providers/userProvider.tsx
+++ src/components/providers/userProvider.tsx
@@ -6,19 +6,55 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [didEnterName, setDidEnterName] = useState(false);
   const [name, setName] = useState("");
   const [gameboard, setGameboard] = useState(functions.getGameBoard());
-  const [lastThree, setLastThree] = useState([]);
-  const [numTurns, setNumTurns] = useState(1);
+  const [lastThreeO, setLastThreeO] = useState<number[]>([]);
+  const [lastThreeX, setLastThreeX] = useState<number[]>([]);
+  const [numTurns, setNumTurns] = useState(2);
 
   const x = "\uf00d";
   const o = "\u004f";
 
   const changeTile = (id: number) => {
-    if (numTurns < 4) {
-      const userTile = gameboard[id].id;
-      const availableTiles = gameboard.filter((tile) => tile.id !== id);
-
-      const cpuChoice =
+    const userTile = gameboard[id].id;
+    const newLastThreeX = [...lastThreeX, userTile];
+    const afterXturn = functions.changeGameboardArr(
+      gameboard,
+      x,
+      o,
+      userTile,
+      numTurns
+    );
+    const availableTiles = afterXturn.filter((tile) => tile.owner === "");
+    let cpuChoice: number;
+    const oTurnNumb = numTurns + 1;
+    if (oTurnNumb < 5) {
+      if (availableTiles.filter((tile) => tile.id === 4).length > 0) {
+        cpuChoice = 4;
+        setLastThreeO([...lastThreeO, cpuChoice]);
+        const cpuTurnArr = functions.changeGameboardArr(
+          afterXturn,
+          x,
+          o,
+          cpuChoice,
+          oTurnNumb
+        );
+        setGameboard(cpuTurnArr);
+        setNumTurns(oTurnNumb + 1);
+        return;
+      }
+      cpuChoice =
         availableTiles[Math.floor(Math.random() * availableTiles.length)].id;
+
+      setLastThreeO([...lastThreeO, cpuChoice]);
+      const cpuTurnArr = functions.changeGameboardArr(
+        afterXturn,
+        x,
+        o,
+        cpuChoice,
+        oTurnNumb
+      );
+      setGameboard(cpuTurnArr);
+      setNumTurns(oTurnNumb + 1);
+      return;
     }
   };
 
--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -12,6 +12,23 @@ const getGameBoard = (): Array<gameTile> => {
   return tiles;
 };
 
+const changeGameboardArr = (
+  gameboard: Array<gameTile>,
+  x: string,
+  o: string,
+  id: number,
+  turns: number
+) => {
+  return gameboard.map((tile) => {
+    if (tile.id === id) {
+      return { ...tile, owner: turns % 2 === 0 ? x : o };
+    } else {
+      return tile;
+    }
+  });
+};
+
 export const functions = {
   getGameBoard,
+  changeGameboardArr,
 };
